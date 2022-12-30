9cab697ecce1f7fa90ce6b7b2df34e4602d5a230
AbyssWatchers <2254347232@qq.com>
2022-12-30 19:29:10 +0800

    消息总线

---
+++ cloud-config-client3366/src/main/java/com/abyss/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,18 @@
+package com.abyss.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author Abyss Watchers
+ * @create 2022-12-30 18:22
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class, args);
+    }
+}
